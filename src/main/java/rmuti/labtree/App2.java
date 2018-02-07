6ab237683d4a843f1ceee17f96b7d196b6bc2d9b
KingCop <copterlovenfsmw2012@gmail.com>
2018-02-07 15:06:10 +0700

    LabBinaryTree

---
+++ src/main/java/rmuti/labtree/App2.java
@@ -0,0 +1,24 @@
+package rmuti.labtree;
+
+import rmuti.ds.Node;
+import rmuti.ds.TreeDraw;
+
+public class App2 {
+	
+public static void main( String[]args) {
+	Node h = new Node("h", null, null);
+	Node i = new Node("i", null, null);
+	Node e = new Node("e", h, i);
+	Node d = new Node("f", null, null);
+	Node b = new Node("b", e, d);
+	Node g = new Node("g", null, null);
+	Node f = new Node("f", null, null);
+	Node c = new Node("c", g, f);
+	
+	
+	Node root = new Node("a", b, c);
+	
+	TreeDraw drawTree = new TreeDraw(root);
+	drawTree.binaryTreeDraw();
+}
+}
